906b73e61af05586e70943bb6e09c36b18a43bde
pparry <1170015014@qq.com>
2020-04-24 17:25:10 +0800

    vue water

---
+++ canvas/water.vue
@@ -0,0 +1,64 @@
+<template>
+    <canvas ref="water"></canvas>
+</template>
+<script>
+export default {
+    name: 'Water',
+    data(){
+        return {
+
+        }
+    },
+    mounted(){
+        this.init();
+    },
+    methods: {
+        init(){
+            let canvas = this.$refs['water'];
+            let ctx = canvas.getContext('2d');  
+            canvas.width = canvas.parentNode.offsetWidth;  
+            canvas.height = canvas.parentNode.offsetHeight;
+            //如果浏览器支持requestAnimFrame则使用requestAnimFrame否则使用setTimeout  
+            window.requestAnimFrame = (function(){  
+            return  window.requestAnimationFrame       ||  
+                    window.webkitRequestAnimationFrame ||  
+                    window.mozRequestAnimationFrame    ||  
+                    function( callback ){  
+                    window.setTimeout(callback, 1000 / 60);  
+                    };  
+            })();  
+            // 波浪大小
+            let boHeight = canvas.height / 10;
+            let posHeight = canvas.height / 1.2;
+            //初始角度为0  
+            let step = 0;  
+            //定义三条不同波浪的颜色  
+            let lines = ["rgba(0,222,255, 0.2)",  
+                        "rgba(157,192,249, 0.2)",  
+                        "rgba(0,168,255, 0.2)"];  
+            function loop(){  
+                ctx.clearRect(0,0,canvas.width,canvas.height);  
+                step++;  
+                //画3个不同颜色的矩形  
+                for(let j = lines.length - 1; j >= 0; j--) {  
+                    ctx.fillStyle = lines[j];  
+                    //每个矩形的角度都不同，每个之间相差45度  
+                    let angle = (step+j*50)*Math.PI/180;  
+                    let deltaHeight   = Math.sin(angle) * boHeight;
+                    let deltaHeightRight   = Math.cos(angle) * boHeight;  
+                    ctx.beginPath();
+                    ctx.moveTo(0, posHeight+deltaHeight);  
+                    ctx.bezierCurveTo(canvas.width/2, posHeight+deltaHeight-boHeight, canvas.width / 2, posHeight+deltaHeightRight-boHeight, canvas.width, posHeight+deltaHeightRight);  
+                    ctx.lineTo(canvas.width, canvas.height);  
+                    ctx.lineTo(0, canvas.height);  
+                    ctx.lineTo(0, posHeight+deltaHeight);  
+                    ctx.closePath();  
+                    ctx.fill();  
+                }
+                window.requestAnimFrame(loop);
+            }  
+            loop(); 
+        }
+    }
+}
+</script>
